a374eb6f0eb6626b025802c512d051d15326c2ba
Loris Matteri <loris.matteri93@gmail.com>
2020-12-17 11:04:31 +0100

    Messaggio in mancanza di task
    Con v-if e v-else inserito messaggio "nulla da fare" qualora venissero cancellate tutte le task nella lista.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,14 @@
 //Istruzioni:
 //Create una todo list usando VueJS.
 //Funzionalitá:
-//La nostra todo list avrá alcune tasks di default predefinite
-//L'utente puó inserire nuove tasks
-//Cliccando sulla "X" l'utente puó cancellare una task
 //Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
-//Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
 //Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
 
 let app = new Vue({
     el: '#app',
     data:{
         task: "",
+        //La nostra todo list avrá alcune tasks di default predefinite
         tasks: [
             'Cosa 1',
             'Cosa 2',
@@ -19,18 +16,21 @@ let app = new Vue({
         ]
     },
     methods: {
+        //L'utente puó inserire nuove tasks
         aggiungi: function() {
             this.tasks.push(this.task);
             console.log(this.task);
             this.task = "";
         },
+        //Cliccando sulla "X" l'utente puó cancellare una task
         togli: function(index) {
             this.tasks.splice(index, 1);
         }
     },
+    //Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
     mounted() {
         document.addEventListener('keyup', e => {
-            console.log(e);
+            
             if(e.key === "Enter"){
                 this.aggiungi()
             }
